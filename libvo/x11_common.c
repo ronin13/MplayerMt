44b8d50b348eb338e846c4cc0bc85a6d69808a41
Raghavendra D Prabhu <raghu.prabhu13@gmail.com>
2011-01-02 14:18:45 +0530

    Removed the XNextEvent part in vo create window.
    On multi-workspace window managers, when mplayer is spawned on a workspace
    not visible currently, it doesn't play or respond to other commands, say in slave mode.
    After running ltrace, mplayer was spinning at XNextEvent waiting for a MapNotify event.
    Now window managers won't send this event until and unless the workspace is visible.
    So changing the vo create function to not spin there. Tested with vdpau,gl VOs.

--- libvo/x11_common.c
+++ libvo/x11_common.c
@@ -1105,9 +1105,9 @@ void vo_x11_create_vo_window(struct vo *vo, XVisualInfo *vis, int x, int y,
     XMapWindow(mDisplay, x11->window);
     vo_x11_clearwindow(vo, x11->window);
     // wait for map
-    do {
-      XNextEvent(mDisplay, &xev);
-    } while (xev.type != MapNotify || xev.xmap.event != x11->window);
+//    do {
+//      XNextEvent(mDisplay, &xev);
+//    } while (xev.type != MapNotify || xev.xmap.event != x11->window);
     XSelectInput(mDisplay, x11->window, NoEventMask);
     XSync(mDisplay, False);
     vo_x11_selectinput_witherr(mDisplay, x11->window,
